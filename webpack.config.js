246a48b22c338c9889f27470bd79f6db0d41a0e0
LarsVanderheydt <lars06@hotmail.be>
2018-01-30 22:38:05 +0100

    link change

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ const {ifProduction, ifDevelopment} = getIfUtils(process.env.NODE_ENV);
 const extractCSS = new ExtractTextWebpackPlugin(`css/style.css`);
 
 // change for production build on different server path
-const publicPath = `/`;
+const publicPath = `https://babiesworld.herokuapp.com/`;
 
 const port = 3000;
 

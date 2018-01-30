ea93c22c9d3ceb26ef637263e0cf8259fc393437
LarsVanderheydt <lars06@hotmail.be>
2018-01-30 23:30:08 +0100

    production + basic server

---
+++ app.js
@@ -0,0 +1,36 @@
+const Hapi = require(`hapi`);
+const inert = require(`inert`);
+const Path = require(`path`);
+
+// const server = new Hapi.Server({ port: 3000, host: 'localhost' });
+const server = new Hapi.Server({
+  port: process.env.PORT || 3000,
+  routes: {
+    files: {
+      relativeTo: Path.join(__dirname, `dist`)
+    }
+  }
+});
+
+const provision = async () => {
+
+  await server.register(inert);
+
+  server.route({
+    method: `GET`,
+    path: `/{param*}`,
+    handler: {
+      directory: {
+        path: `.`,
+        redirectToSlash: true,
+        index: true,
+      }
+    }
+  });
+
+  await server.start();
+
+  console.log(`Server running at:`, server.info.uri);
+};
+
+provision();
--- src/js/containers/Character.jsx
+++ src/js/containers/Character.jsx
@@ -66,7 +66,7 @@ const Character = ({selectType, setType, previewTypes, character, place}) => {
       <div className='icons_div'>
         {
           previewTypes.map(icon => {
-            return <img key={icon.type} src={`./assets/img/Icons/${icon.type}.png`} width='35' height='35' onClick={handleIconClick} id={icon.type} className={selectType === icon.type ? `currentIcon` : `icon`} />;
+            return <img key={icon.type} src={`https://babiesworld.herokuapp.com/assets/img/Icons/${icon.type}.png`} width='35' height='35' onClick={handleIconClick} id={icon.type} className={selectType === icon.type ? `currentIcon` : `icon`} />;
           })
         }
       </div>

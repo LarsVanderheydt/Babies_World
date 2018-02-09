f451208bfdf8d2c1ea0cafed7735101fa0b602a8
LarsVanderheydt <lars06@hotmail.be>
2018-02-09 12:57:50 +0100

    partner next button fix

--- src/js/components/Partner/index.js
+++ src/js/components/Partner/index.js
@@ -45,9 +45,14 @@ const Partner = ({partner, setPartner}) => {
         </g>
       </svg>
 
+
       <svg width='9px' height='14px' viewBox='0 0 9 14' version='1.1' xmlns='http://www.w3.org/2000/svg' onClick={onNextClick}>
-        <g id='' transform='rotate(90.000000) translate(-2.000000, 3.000000)' fill='#9B1B51'>
-          <polygon id='Back-button' transform='rotate(90.000000)' points='2.63095238 3.84615385 9.13095238 -2.61538462 10.3690476 -1.38461538 5.10714286 3.84615385 10.3690476 9.07692308 9.13095238 10.3076923'></polygon>
+        <g id='Unicorn' transform='translate(-327.000000, -332.000000)' fill='#9B1B51'>
+            <g id='Group' transform='translate(39.000000, 332.000000)'>
+                <g id='Icons/iMessage/Back' transform='translate(292.500000, 7.000000) rotate(90.000000) translate(-292.500000, -7.000000) translate(286.000000, 3.000000)'>
+                  <polygon transform='translate(6.500000, 3.846154) rotate(90.000000) translate(-6.500000, -3.846154) ' points='2.63095238 3.84615385 9.13095238 -2.61538462 10.3690476 -1.38461538 5.10714286 3.84615385 10.3690476 9.07692308 9.13095238 10.3076923'></polygon>
+                </g>
+            </g>
         </g>
       </svg>
 
